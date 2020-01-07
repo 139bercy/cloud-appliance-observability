712e735fe85376844170d1c5edae0e4004180c10
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-01-07 13:30:14 +0100

    [logs] check playbook result step by step

--- logs/graylog.appliance.autoconf.sh
+++ logs/graylog.appliance.autoconf.sh
@@ -5,14 +5,18 @@ ansible-galaxy install -r $ETC_PATH/appliance.ansible_requirements.yml
 ansible-playbook -t os-ready $PLAYBOOK \
 	-e dnsmasq_listening_interfaces="{{['lo']|from_yaml}}" \
 	-e bootstrap_http_proxy="$HTTP_PROXY" \
-	-e bootstrap_no_proxy="$NO_PROXY"
+	-e bootstrap_no_proxy="$NO_PROXY" \
+	|| exit 1
 
 ansible-playbook -t docker $PLAYBOOK \
 	-e docker_http_proxy="$HTTP_PROXY" \
-	-e docker_no_proxy="$NO_PROXY"
+	-e docker_no_proxy="$NO_PROXY" \
+	|| exit 1
 
-ansible-playbook -t elasticsearch $PLAYBOOK
+ansible-playbook -t elasticsearch $PLAYBOOK \
+	|| exit 1
 
 ansible-playbook -t graylog $PLAYBOOK \
 	-e graylog_login_admin_local="$GRAYLOG_ADMIN_NAME" \
-	-e graylog_root_password="$GRAYLOG_ADMIN_PASSWORD"
+	-e graylog_root_password="$GRAYLOG_ADMIN_PASSWORD" \
+	|| exit 1

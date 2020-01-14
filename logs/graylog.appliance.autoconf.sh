d5fccd5d0e4b28933f12b8412d2bb9825068f950
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-01-14 16:33:41 +0100

    [cinder] trigger udev discovery

--- logs/graylog.appliance.autoconf.sh
+++ logs/graylog.appliance.autoconf.sh
@@ -3,6 +3,9 @@
 ansible-galaxy install -r $ETC_PATH/appliance.ansible_requirements.yml
 ansible-galaxy install -r $ETC_PATH/graylog.ansible_requirements.yml
 
+# Sometimes cinder volumes are not linked against /dev/disk/by-id/
+udevadm trigger
+
 ansible-playbook -t os-ready $PLAYBOOK \
 	-e dnsmasq_listening_interfaces="{{['lo']|from_yaml}}" \
 	-e bootstrap_http_proxy="$HTTP_PROXY" \

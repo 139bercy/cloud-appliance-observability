b882578559018233f9c1e09b78597fa606b366e0
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-01-15 16:04:52 +0100

    [metrics] First version

---
+++ metrics/metrics.appliance.autoconf.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env/bash
+
+ansible-galaxy install -r $ETC_PATH/appliance.ansible_requirements.yml
+ansible-galaxy install -r $ETC_PATH/metrics.ansible_requirements.yml
+
+# Sometimes cinder volumes are not linked against /dev/disk/by-id/
+udevadm trigger
+
+ansible-playbook -t os-ready $PLAYBOOK \
+	-e dnsmasq_listening_interfaces="{{['lo']|from_yaml}}" \
+	-e bootstrap_http_proxy="$HTTP_PROXY" \
+	-e bootstrap_no_proxy="$NO_PROXY" \
+	|| exit 1
+
+ansible-playbook -t containers $PLAYBOOK \
+	-e podman_http_proxy="$HTTP_PROXY" \
+	-e podman_no_proxy="$NO_PROXY" \
+	|| exit 1
+
+ansible-playbook -t metrics $PLAYBOOK \
+	|| exit 1

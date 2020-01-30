d4bd8604dced6a844f2ad242b7daf85172fc7512
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-01-30 10:39:32 +0100

    [ansible] add post-configuration tasks to autoconf

--- metrics/metrics.appliance.autoconf.sh
+++ metrics/metrics.appliance.autoconf.sh
@@ -17,6 +17,6 @@ ansible-playbook -t containers $PLAYBOOK \
 	-e podman_no_proxy="$NO_PROXY" \
 	|| exit 1
 
-ansible-playbook -t metrics $PLAYBOOK \
+ansible-playbook -t metrics,configuration $PLAYBOOK \
 	-e@$ETC_PATH/metrics.variables.yml \
 	|| exit 1

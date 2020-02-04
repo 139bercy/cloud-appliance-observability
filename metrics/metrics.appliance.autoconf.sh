625af29d69881e171ec51af12de73daa6d0be7b1
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-02-04 15:23:11 +0100

    [autoconf] set openstack credentials

--- metrics/metrics.appliance.autoconf.sh
+++ metrics/metrics.appliance.autoconf.sh
@@ -25,7 +25,18 @@ export INFLUXDB_ADMIN_PASSWORD=$INFLUXDB_ADMIN_PASSWORD
 export INFLUXDB_ORG=$INFLUXDB_ORG
 export INFLUXDB_RETENTION_HOURS=$INFLUXDB_RETENTION_HOURS
 
-/root/appliance/metrics/metrics.appliance.autoconf.sh
+export OS_AUTH_URL=$OS_AUTH_URL
+export OS_IDENTITY_API_VERSION=$OS_IDENTITY_API_VERSION
+export OS_INTERFACE=$OS_INTERFACE
+export OS_PASSWORD=$OS_PASSWORD
+export OS_PROJECT_DOMAIN_ID=$OS_PROJECT_DOMAIN_ID
+export OS_PROJECT_ID=$OS_PROJECT_ID
+export OS_PROJECT_NAME=$OS_PROJECT_NAME
+export OS_REGION_NAME=$OS_REGION_NAME
+export OS_USERNAME=$OS_USERNAME
+export OS_USER_DOMAIN_NAME=$OS_USER_DOMAIN_NAME
+
+. /root/appliance/metrics/metrics.appliance.autoconf.sh
 EOF
 
 ansible-galaxy install -r $ETC_PATH/appliance.ansible_requirements.yml

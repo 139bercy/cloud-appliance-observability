a29c1a1fbf03dbf1b9f9781d60f5f1d07fc3e33b
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-02-04 14:13:20 +0100

    [autoconf] create reconf script for debugging

--- metrics/metrics.appliance.autoconf.sh
+++ metrics/metrics.appliance.autoconf.sh
@@ -1,5 +1,33 @@
 #! /usr/bin/env/bash
 
+cat > /root/appliance/metrcis/metrics.appliance.reconf.sh <<EOF
+export ETC_PATH=$ETC_PATH
+export REPO_PATH=$REPO_PATH
+
+export HTTP_PROXY=$HTTP_PROXY
+export NO_PROXY=$NO_PROXY
+export http_proxy=$http_proxy
+export no_proxy=$no_proxy
+
+export PLAYBOOK=$PLAYBOOK
+
+export CONTAINERS_VOLUME=$CONTAINERS_VOLUME
+export METRICS_VOLUME=$METRICS_VOLUME
+
+export METRICS_ENDPOINT_URL=$METRICS_ENDPOINT_URL
+export METRICS_CONTAINER=$METRICS_CONTAINER
+
+export GRAFANA_ADMIN_NAME=$GRAFANA_ADMIN_NAME
+export GRAFANA_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD
+
+export INFLUXDB_ADMIN_NAME=$INFLUXDB_ADMIN_NAME
+export INFLUXDB_ADMIN_PASSWORD=$INFLUXDB_ADMIN_PASSWORD
+export INFLUXDB_ORG=$INFLUXDB_ORG
+export INFLUXDB_RETENTION_HOURS=$INFLUXDB_RETENTION_HOURS
+
+/root/appliance/metrics/metrics.appliance.autoconf.sh
+EOF
+
 ansible-galaxy install -r $ETC_PATH/appliance.ansible_requirements.yml
 ansible-galaxy install -r $ETC_PATH/metrics.ansible_requirements.yml
 

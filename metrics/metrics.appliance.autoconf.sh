654fbe9515f43fe20f452756e793abecbe05c414
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2020-02-17 16:48:21 +0100

    [metrics] cleanup scripts

--- metrics/metrics.appliance.autoconf.sh
+++ metrics/metrics.appliance.autoconf.sh
@@ -50,9 +50,6 @@ EOF
 ansible-galaxy install -r $ETC_PATH/appliance.ansible_requirements.yml
 ansible-galaxy install -r $ETC_PATH/metrics.ansible_requirements.yml
 
-# Sometimes cinder volumes are not linked against /dev/disk/by-id/
-udevadm trigger
-
 ansible-playbook -t os-ready $PLAYBOOK \
 	-e@$ETC_PATH/metrics.variables.yml \
 	-e dnsmasq_listening_interfaces="{{['lo']|from_yaml}}" \
